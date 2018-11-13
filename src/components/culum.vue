a5327423f70eb53f429610a386336a71e1537b83
Kevin <1025046794@qq.com>
2018-11-13 09:04:52 +0800

    写错分支

--- src/components/culum.vue
+++ src/components/culum.vue
@@ -22,46 +22,55 @@
               <el-table :data="evening" border style="width: 100%">
                 <el-table-column fixed label="教室" width="150">
                   <template slot-scope="scope">
-                    <el-col :span="2" class="wer-col">
-                        <h4>{{scope.row.name}}</h4>
+                    <el-col class="wer-col">
+                        <h4><!-- {{scope.row.name}} -->教室1</h4>
                     </el-col>
                     <!-- <span style="margin-left: 10px">{{ scope.row.name }}</span> -->
                   </template>
                 </el-table-column>
-                <el-table-column label="时间" width="150">
+                <el-table-column label="时间" width="2000px;">
                   <template slot-scope="scope">
-                    <span style="margin-left: 10px">{{ scope.row.date }}</span>
+                    <!-- <span style="margin-left: 10px">{{ scope.row.date }}</span> -->
+                    <!-- <el-col :span="1" class="wer-col2" v-for="(value,index) in scope.row.time" :key="index" > -->
+                      <div v-for="(value,index) in scope.row.time" :key="index" style="display:inline-block">
+                        <div class="wer-div" >
+                          {{value.time.substring(0,5)}}
+                        </div>
+                        <span style="font-size:12px;">{{value.staff}}·{{value.name}}</span>
+                      </div>
+
+                    <!-- </el-col> -->
                   </template>
                 </el-table-column>
               </el-table>
             </template>
-            <el-row>
+            <!-- <el-row>
                 <el-col :span="24">
                     <el-tabs v-model="activeName2" type="card" @tab-click="handleClick">
                         <template>
                             <div class="morning-main3">
                                 <el-col :span="24" v-for="item in evening" :key="item.id"  style="overflow:scroll;">
                                     <div class="mor-wer">
-                                        <!-- <el-col :span="2" class="wer-col">
+                                        <el-col :span="2" class="wer-col">
                                             <h4>{{item.name}}</h4>
                                         </el-col>
                                         <el-col :span="2" class="wer-col2" v-for="(value,index) in item.time" :key="index">
                                             <div class="wer-div">{{value.time}}</div>
                                             <span>{{value.staff}}·{{value.name}}</span>
-                                        </el-col> -->
-                                        <!-- <el-col :span="2" class="wer-col2">
+                                        </el-col>
+                                        <el-col :span="2" class="wer-col2">
                                             <div class="wer-div2">{{value.time}}</div>
                                         </el-col>
                                         <el-col :span="2" class="wer-col2">
                                             <div class="wer-div3">{{value.time}}</div>
-                                        </el-col> -->
+                                        </el-col>
                                     </div>
                                 </el-col>
                             </div>
                         </template>
                     </el-tabs>
                 </el-col>
-            </el-row>
+            </el-row> -->
         </div>
     </div>
 </template>
@@ -145,7 +154,6 @@ export default {
           ]
         }
       ],
-
       value6: "",
       value1: "",
       value: ""
@@ -198,4 +206,49 @@ export default {
 <style lang="scss" scoped>
 @import "@/styles/privateculum.scss";
 @import "@/styles/culum.scss";
+
+            // 111
+            .wer-div {
+              width: 40px !important;
+              height: 25px !important;
+              background: #00bc71;
+              border-radius: 16px;
+              color: #fff;
+              font-size: 12px;
+              margin: 6px auto;
+              text-align: center;
+              line-height: 27px !important;
+              border: 1px solid #e8e8e8;
+            }
+
+            .wer-div2 {
+              width: 40px !important;
+              height: 25px !important;
+              background: #f5f5f5;
+              border-radius: 16px;
+              color: #8c8c8c;
+              font-size: 12px;
+              margin: 6px auto;
+              text-align: center;
+              line-height: 27px !important;
+              border: 1px solid #e8e8e8;
+            }
+
+            .wer-div3 {
+              width: 40px !important;
+              height: 25px !important;
+              background: #f5f5f5;
+              border-radius: 16px;
+              color: #8c8c8c;
+              font-size: 12px;
+              margin: 6px auto;
+              text-align: center;
+              line-height: 27px !important;
+              border: 1px solid #e8e8e8;
+            }
+
+            span {
+              color: #c7c7c7;
+              font-size: 10px;
+            }
 </style>
