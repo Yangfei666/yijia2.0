468312f8e0bb2cf6a16e731933d9d194723d8f6a
chm <chn@qq.com>
2019-06-29 14:50:45 +0800

    测试一下

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_ROOT: '"//api.yijiayoga.com/pc/v1/"'
-  // API_ROOT: '"http://www.loelve.xyz/pc/v1/"'
+  // API_ROOT: '"//api.yijiayoga.com/pc/v1/"'
+  API_ROOT: '"http://www.loelve.xyz/pc/v1/"'
 }
